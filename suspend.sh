451d871be841a2e0302a04ec26899912137190db
Pramodh M <54981492+pramodhm112@users.noreply.github.com>
2021-05-07 11:19:41 +0530

    Update suspend.sh

--- suspend.sh
+++ suspend.sh
@@ -5,5 +5,8 @@ read -p 'Enter GitHub Hostname : ' hostname
 
 while read line || [ -n "$line" ]
 do
-  curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${token}" "http(s)://${hostname}/api/v3/users/${line}/suspended" -d '{"reason":"inactive"}'
+
+  data=$(echo $line | awk -F "," '{print $1}')
+  echo $data
+  curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${token}" "https://${hostname}/api/v3/users/${data}/suspended" -d '{"reason":"inactive"}'
 done < "inactiveusers.csv"
